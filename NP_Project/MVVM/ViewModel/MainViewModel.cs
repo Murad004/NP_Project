1889625070e6e47d0e2fe6a6285e332f3c97ab16
Murad004 <sadixovmurad322@gmail.com>
2021-12-29 18:45:26 +0400

    Add project files.

---
+++ NP_Project/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP_Project.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+
+
+        public MainViewModel(MainWindow mainWindow)
+        {
+
+        }
+    }
+}
